55342bd529ac05dd4d476cee192cc84fe8b7ab79
Krzysztof Iwanowski <kiwanowski0@gmail.com>
2024-12-24 08:47:22 +0100

    brought back neopixel support

--- code.py
+++ code.py
@@ -4,10 +4,16 @@ import usb_hid
 from adafruit_hid.mouse import Mouse
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_debouncer import Debouncer
+import neopixel
 
 offset = 5
 interval = 180
+counter = 0
 enabled = False
+
+green = (10, 0, 0)
+red = (0, 10, 0)
+colors = [red, green]
 
 staticpin = DigitalInOut(board.GP11)
 staticpin.direction = Direction.OUTPUT
@@ -19,6 +25,8 @@ btn.pull = Pull.DOWN
 switch = Debouncer(btn)
 
 mouse = Mouse(usb_hid.devices)
+pixels = neopixel.NeoPixel(board.GP16, 1)
+pixels[0] = red
 
 now = time.monotonic()
 
@@ -27,6 +35,10 @@ while True:
 
     if switch.rose:
         enabled = not enabled
+        counter += 1
+        if counter > 1:
+            counter = 0
+        pixels[0] = colors[counter]
 
     if (now + interval) < time.monotonic() and enabled:
         mouse.move(0, offset)
